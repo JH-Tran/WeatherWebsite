c0af81c1cb9d373e399253fda649a5855a415696
Justin Tran <jhtran023@gmail.com>
2023-06-21 19:16:59 +1000

    Update cloudiness forecast label

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -59,7 +59,7 @@ const Forecast = ({ data }) => {
                 </div>
                 <div className="daily-details-grid-item">
                   <label>Clouds</label>
-                  <label>{item.clouds.all}</label>
+                  <label>{item.clouds.all}%</label>
                 </div>
                 <div className="daily-details-grid-item">
                   <label>Humidity</label>
